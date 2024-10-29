3d18cf37d1f55d1e9ecaad928636af329a1a9a48
LiahMaksim <maksimliah777@gmail.com>
2024-10-29 14:05:14 +0300

    Create PZ_5_2.py

---
+++ PZ_5_2.py
@@ -0,0 +1,19 @@
+def cut_rectangle_into_squares(A, B):
+    if not (isinstance(A, int) and isinstance(B, int)) or A <= 0 or B <= 0:
+        raise ValueError("Длины сторон A и B должны быть натуральными числами (положительными числами).")
+    count = 0
+    while A > 0 and B > 0:
+        if A > B:
+            count += A // B
+            A = A % B
+        else:
+            count += B // A
+            B = B % A
+    return count
+try:
+    A = int(input("Ввежите длину стороны A (натуральное число): "))
+    B = int(input("Введите длину стороны B (натуральное число): "))
+    result = cut_rectangle_into_squares(A, B)
+    print(f"Количество квадратов, на которое можно разрезать прямоугольник: {result}")
+except ValueError as e:
+    print(e)
