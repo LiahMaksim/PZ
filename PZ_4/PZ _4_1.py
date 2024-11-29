705cdf02c3d657dd623d4dd07d612566837f8761
LiahMaksim <maksimliah777@gmail.com>
2024-11-29 20:19:21 +0300

    Create PZ _4_1.py

---
+++ PZ_4/PZ _4_1.py
@@ -0,0 +1,16 @@
+#Дано вещественное число A и целое число N (>0). Используя один цикл, найти сумму 1 + A + A2 + A3 + ... + AN
+
+def sum_series(A, N):
+    total_sum = 0
+    current_term = 1
+    for i in range(N + 1):
+        total_sum += current_term
+        current_term *= A
+    return total_sum
+A = int(input("Введите вещественное число A: "))
+N = int(input("Введите целое число N (>0): "))
+if N > 0:
+    result = sum_series(A, N)
+    print(f"Сумма: {result} ")
+else:
+    print(f"N должно быть больше 0.")
