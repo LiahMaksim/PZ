58b33335989c5949dc02b09a827ac9a2af552f05
LiahMaksim <maksimliah777@gmail.com>
2024-11-29 20:19:39 +0300

    Create PZ_5_1.py

---
+++ PZ_5/PZ_5_1.py
@@ -0,0 +1,13 @@
+#Составить функцию, которая выведет на экран строку, содержащую задаваемое с клавиатуры число символов.
+
+def print_custom_string():
+    try:
+        fan = int(input("Введите количество символов: "))
+        if fan <= 0:
+            print("Количество символов должно быть положительным. ")
+            return
+        result_string = '*' * fan
+        print(result_string)
+    except ValueError:
+        print("Пожалуйста, введите целое число.")
+print_custom_string()
