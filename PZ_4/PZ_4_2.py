9711338a542e0a2c327c324cf01b052ecb725490
LiahMaksim <maksimliah777@gmail.com>
2024-12-07 12:06:17 +0300

    Update PZ_4_2.py

--- PZ_4/PZ_4_2.py
+++ PZ_4/PZ_4_2.py
@@ -1,14 +1,14 @@
 #Дано целое число N (>0), являющееся некоторой степенью числа 2: N = 2K. Найти целое число K — показатель этой степени.
 
 import math
-def find_exponent(N):
+def front(N):
     if N <= 0 or (N & (N - 1)) != 0:
         raise ValueError("N должно быть положительным и являться степенью двойки.")
     K = int(math.log2(N))
     return K
 try:
     N = int(input("Введите целое число N (>0), являющееся двойки: "))
-    K = find_exponent(N)
+    K = front(N)
     print(f"K ={K}")
 except ValueError:
     print()
